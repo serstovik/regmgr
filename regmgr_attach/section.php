9bc69662bcf38904fc5d477651bbd0becb8dbc6a
SerStoVik <serstovik@gmail.com>
2018-12-28 13:30:19 +0200

    attach section added to backend

---
+++ regmgr_attach/section.php
@@ -0,0 +1,16 @@
+<?php
+/** //** ----= CLASS mwRegmgr_attachInit=------------------------------------------------------------------------------\**//** \
+ *
+ * @package        Morweb
+ * @subpackage
+ * @category
+ *
+ * \**//** ---------------------------------------------------------------------= by SerStoVik @ Morad Media Inc. =----/** //**/
+class mwRegmgr_attachInit extends mwSectionInit {
+
+	public	$Title		= 'RegMgr Attachments';
+	public 	$Description	= 'Attachments extension for regMgr';
+
+	public	$Privileges	= [];
+
+} //CLASS mwRegmgr_attachInit
