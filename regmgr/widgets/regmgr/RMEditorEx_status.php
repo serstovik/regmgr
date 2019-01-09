8dc6956dc92f864d1068e0d56a8c0e0e1b213ad1
Bloody <ukamerzacina@gmail.com>
2019-01-09 21:12:18 +0300

    * improved config support + navigation config

--- regmgr/widgets/regmgr/RMEditorEx_status.php
+++ regmgr/widgets/regmgr/RMEditorEx_status.php
@@ -7,10 +7,6 @@
 class mwRMEditorEx_status extends mwRMEditorEx
 {
 
-	public	$tabs	= [
-		'approval'	=> 'Approval',
-	]; //$tabs
-
 	function editor_approval () {
 		
 		//__($this->application->statusList);
@@ -19,6 +15,38 @@ class mwRMEditorEx_status extends mwRMEditorEx
 		//$this->load->model('rmCfg'); // how to load lib?
 		
 		$statuses = rmCfg()->getStatuses();
+		
+		//default statuses names
+		$coreDefault = [
+			'new'		=> 'New',
+			'open'		=> 'Open',
+			'submit'	=> 'Submit',
+			'ready'		=> 'Ready',
+			'approved'	=> 'Approved',
+			'declined'	=> 'Declined',
+			'closed'	=> 'Closed',
+		];
+		
+		foreach($coreDefault as $k => $v){
+			
+			//check is core status exists in config
+			if ( !empty($statuses[$k]) ) {
+				
+				//use caption from config
+				$coreList[$k] = $statuses[$k];
+				
+				//remove core status from config list
+				unset($statuses[$k]);
+				
+			}
+			else {
+				
+				//use default caption
+				$coreList[$k] = $coreDefault[$k];
+				
+			}
+		}
+		
 		$statuses = array_merge([0 => 'Select Status'], $statuses);
 		//__($statuses);
 		
@@ -33,6 +61,7 @@ class mwRMEditorEx_status extends mwRMEditorEx
 					<!--input id="regmgr_approval_value" type="hidden" name="approval_value" /-->
 				</td></tr>
 				
+				<?php if( sizeof($statuses) > 1 ):?>
 				<tr><th>Custom Statuses:</th></tr>
 				<tr><td>
 					<select name="status_minor" id="regmgr_minor_status" onChange="jQuery('[name=status_minor]').val(jQuery(this).val());">
@@ -41,32 +70,26 @@ class mwRMEditorEx_status extends mwRMEditorEx
 					<?endforeach;?>
 					</select>
 				</td></tr>
+				<?php endif;?>
 			</table>
 		</div>
 		<script>
 		jQuery(function(){
 			//___(11111);
+			
 			var update_text = function() {
 				
-				var val = jQuery('[name=statusmajor]').val();
+				var val = jQuery('[name=status_major]').val();
 				
 				var text = jQuery('#regmgr_approve_text');
 				
-				var statuses = {
-					'new'		: 'New',
-					'open'		: 'Open',
-					'submit'	: 'Submit',
-					'ready'		: 'Ready',
-					'approved'	: 'Approved',
-					'declined'	: 'Declined',
-					'closed'	: 'Closed',
-				};
+				var statuses = <?=json_encode($coreList)?>;
 				
 				text.html(statuses[val]);
 					
 			};
 			
-			jQuery('[rel=status_approval]').off().click(function(){
+			jQuery('[rel=tab_status]').off().click(function(){
 				
 				//update major status text
 				update_text();
@@ -75,7 +98,7 @@ class mwRMEditorEx_status extends mwRMEditorEx
 			
 			regmgr_update_approve = function(val) {
 				
-				jQuery('[name=statusmajor]').val(val);
+				jQuery('[name=status_major]').val(val);
 				update_text();
 				
 			};
